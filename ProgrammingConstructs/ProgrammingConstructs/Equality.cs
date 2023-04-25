e32ddc815f02142ef0ebca3cc46fb654a21adeef
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-04-25 17:53:01 +0530

    [Ankita] Add : UC1-Check Two Integers Are Equal Or Not

---
+++ ProgrammingConstructs/ProgrammingConstructs/Equality.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructs
+{
+    public class Equality
+    {
+       public int num_One,num_Two;
+        public void CompareNo()
+        {
+            Console.WriteLine("Enter First No");
+            num_One=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second No");
+            num_Two = Convert.ToInt32(Console.ReadLine());
+             if (num_One == num_Two)
+            {
+                Console.WriteLine("Both Numbers Are Equal");
+            }
+             else
+            {
+                Console.WriteLine("Both Numbers Are Not Equal");
+            }
+
+        }
+    }
+   
+}
---
+++ ProgrammingConstructs/ProgrammingConstructs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingConstructs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Equality obj = new Equality();
+            obj.CompareNo();
+        }
+    }
+}
