aaecff970886d96d07496d17b6476ffdf5b2ec9d
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-04-27 15:04:14 +0530

    [Anjita] Add : UC8-Print Sum Of Square

--- ProgrammingConstructs/ProgrammingConstructs/Equality.cs
+++ ProgrammingConstructs/ProgrammingConstructs/Equality.cs
@@ -8,20 +8,23 @@ namespace ProgrammingConstructs
 {
     public class Equality
     {
-             public int i;
-        public void PowerOfTwo()
+
+        public int n, sum = 0;
+        public void Square()
         {
-            Console.WriteLine("Enter the Number : ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            for (i = 1; i <= number; i++)
+            Console.WriteLine("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The squares upto {0} terms are :", n);
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine($"The Table of 2 * {i} = {2 * i}");
+                Console.Write("{0}  ", i * i);
+                sum += i * i;
             }
+            Console.WriteLine("The Sum of Squares of numbers upto {0} terms = {1}", n, sum);
+       
+       
 
-
-        }
-
-            
+        }    
 
         
     }
